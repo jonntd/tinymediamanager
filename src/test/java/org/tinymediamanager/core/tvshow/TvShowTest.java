0867fab208544235b364d96f56ff690ea8b5e1c7
Manuel Laggner <manuel.laggner@gmail.com>
2024-08-23 15:44:45 +0200

    🐛 [UDS] enhanced parsing of season and episode numbers

--- src/test/java/org/tinymediamanager/core/tvshow/TvShowTest.java
+++ src/test/java/org/tinymediamanager/core/tvshow/TvShowTest.java
@@ -169,7 +169,7 @@ public class TvShowTest extends BasicTvShowTest {
     assertEqual("S:1 E:3", detectEpisode("tvp-wildesskandinavien-e03-720p.mkv"));
     assertEqual("S:4 E:13", detectEpisode("s800The Mentalist_S04E13_Die goldene Feder.avi"));
     assertEqual("S:1 E:1", detectEpisode("AwesomeTvShow.S01E01-480p.mkv"));
-    assertEqual("S:7 E:9 E:10", detectEpisode("stvs7ep9-10.avi"));
+    assertEqual("S:7 E:9 E:10", detectEpisode("stv-s7ep9-10.avi"));
     assertEqual("S:1 E:545", detectEpisode("s01e545 - Steamtown USA.mkv")); // http://thetvdb.com/?tab=season&seriesid=188331&seasonid=311381&lid=7
     assertEqual("S:13 E:2 Split", detectEpisode("Doctor.Who.S13.E2.Part4.Planet.of.Evil.DVDRip.XviD-m00tv.avi"));
     assertEqual("S:3 E:5", detectEpisode("vs-once-upon-a-time-_S03XE05_dd51-ded-dl-7p-bd-x264-305.mkv"));
@@ -190,6 +190,7 @@ public class TvShowTest extends BasicTvShowTest {
     assertEqual("S:4 E:101", detectEpisode("Season 4 Episode 101.avi"));
     assertEqual("S:4 E:204", detectEpisode("4x204.avi"));
     assertEqual("S:1 E:13 E:14 E:15", detectEpisode("Peter Pan S01E13_1x14_1x15 - El Hookato.ts"));
+    assertEqual("S:1 E:1", detectEpisode("Better Call Saul - S01E01 - Uno - [2160p-WEB.DL-HEVC.x265-10bit.SDR-DTS.DTS5.1-TrollUHD].mkv"));
 
     // ANIME - https://kodi.wiki/view/Anime
     // PREPEND
