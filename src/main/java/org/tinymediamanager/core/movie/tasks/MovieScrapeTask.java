371ac4e7bdc89a98ad19c2710f2195d0e9e3996a
Myron Boyle <myron0815@gmx.net>
2024-10-15 11:26:41 +0200

    ♿️ [artwork] do not always overwrite IDs, extend them!
    So that our scrapers possibly also are able to crawl for artwork, not
    only the lastUsedScraper with its ID...
    
    (Also set the EG for episodes, as we might react on that)

--- src/main/java/org/tinymediamanager/core/movie/tasks/MovieScrapeTask.java
+++ src/main/java/org/tinymediamanager/core/movie/tasks/MovieScrapeTask.java
@@ -313,7 +313,7 @@ public class MovieScrapeTask extends TmmThreadPool {
       options.setArtworkType(MediaArtworkType.ALL);
       options.setMetadata(metadata);
       if (metadata != null) {
-        options.setIds(metadata.getIds());
+        options.addIds(metadata.getIds());
       }
       if (movie.isStacked()) {
         ArrayList<MediaFile> mfs = new ArrayList<>();
@@ -358,7 +358,7 @@ public class MovieScrapeTask extends TmmThreadPool {
       options.setDataFromOtherOptions(movieScrapeParams.searchAndScrapeOptions);
       options.setMetadata(metadata);
       if (metadata != null) {
-        options.setIds(metadata.getIds());
+        options.addIds(metadata.getIds());
       }
 
       // scrape trailers
