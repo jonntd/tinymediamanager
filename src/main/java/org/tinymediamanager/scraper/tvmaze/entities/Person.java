d1a4a0e4876105ad003f6405347782efcdae8d7e
Myron Boyle <myron0815@gmx.net>
2024-10-24 09:08:24 +0200

    🧑‍💻 [JSON] introduce BaseJsonEntity for TvMaze

--- src/main/java/org/tinymediamanager/scraper/tvmaze/entities/Person.java
+++ src/main/java/org/tinymediamanager/scraper/tvmaze/entities/Person.java
@@ -1,11 +1,13 @@
 package org.tinymediamanager.scraper.tvmaze.entities;
 
-public class Person {
+import org.tinymediamanager.scraper.entities.BaseJsonEntity;
+
+public class Person extends BaseJsonEntity {
   public int       id;
   public String    url;
   public String    name;
   public Country   country;
-  public String    birthdate;
+  public String    birthday;
   public String    deathday;
   public String    gender;
   public ImageUrls image;
