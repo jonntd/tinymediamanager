58be8b6a1ffbbee884d46c166e423efc43030c92
Myron Boyle <myron0815@gmx.net>
2024-12-13 13:28:25 +0100

    🧑‍💻 fix import

--- src/main/java/org/tinymediamanager/core/tvshow/connector/TvShowGenericXmlConnector.java
+++ src/main/java/org/tinymediamanager/core/tvshow/connector/TvShowGenericXmlConnector.java
@@ -43,7 +43,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.lang3.StringUtils;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tinymediamanager.core.CertificationStyle;
@@ -67,6 +66,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * this class is a general XML connector which suits as a base class for most xml based connectors
