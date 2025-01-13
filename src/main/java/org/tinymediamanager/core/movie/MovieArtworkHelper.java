27444fc7a73a9b13c27dc4272e3de46e56f7d832
Myron Boyle <myron0815@gmx.net>
2025-01-13 14:05:12 +0100

    🥅 [images] consider MediaArtworks w/o correctly set ImageSizeAndUrl
    but YES, every usage of MediaArtwork should provide this!!!

--- src/main/java/org/tinymediamanager/core/movie/MovieArtworkHelper.java
+++ src/main/java/org/tinymediamanager/core/movie/MovieArtworkHelper.java
@@ -1122,7 +1122,17 @@ public class MovieArtworkHelper {
     // should we fall back to _any_ artwork?
     if (MovieModuleManager.getInstance().getSettings().isImageScraperFallback()) {
       for (MediaArtwork art : artworkForType) {
-        for (MediaArtwork.ImageSizeAndUrl imageSizeAndUrl : art.getImageSizes()) {
+        if (!art.getImageSizes().isEmpty()) {
+          for (MediaArtwork.ImageSizeAndUrl imageSizeAndUrl : art.getImageSizes()) {
+            if (!sortedArtwork.contains(imageSizeAndUrl)) {
+              sortedArtwork.add(imageSizeAndUrl);
+            }
+          }
+        }
+        else {
+          // we forgot to specify a MA with ImageSizeANdUrl - create a dummy one
+          LOGGER.trace("MediaArtwork w/o imageSizeAndUrl - this should not be! {}", art);
+          MediaArtwork.ImageSizeAndUrl imageSizeAndUrl = new MediaArtwork.ImageSizeAndUrl(0, 0, art.getOriginalUrl());
           if (!sortedArtwork.contains(imageSizeAndUrl)) {
             sortedArtwork.add(imageSizeAndUrl);
           }
