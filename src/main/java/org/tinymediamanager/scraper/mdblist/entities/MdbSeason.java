7df6edd219e31692efbfca5d72ab0a8d8f236954
Myron Boyle <myron0815@gmx.net>
2025-02-21 15:32:01 +0100

    🙈 [mdblist] cannot parse date / unneeded
    fixed in branch, forgotten in devel :)

--- src/main/java/org/tinymediamanager/scraper/mdblist/entities/MdbSeason.java
+++ src/main/java/org/tinymediamanager/scraper/mdblist/entities/MdbSeason.java
@@ -1,13 +1,12 @@
 package org.tinymediamanager.scraper.mdblist.entities;
 
-import java.util.Date;
-
 import org.tinymediamanager.scraper.entities.BaseJsonEntity;
 
 public class MdbSeason extends BaseJsonEntity {
   public int    tmdbid;
   public String name;
-  public Date   air_date;
+  // public Date air_date;
+  public String air_date;     // cannot parse with default typeAdapter
   public int    episode_count;
   public int    season_number;
   public String tomatofresh;
