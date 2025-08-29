349ad9f35f8a1d2cf752a4b67b5f93aaa2ad59be
jonntd <jonntd@gmail.com>
2025-08-29 23:45:44 +0800

    e

--- src/test/resources/powershell with space.ps1
+++
@@ -1,6 +0,0 @@
-write-host "There are a total of $($args.count) arguments"
-for ( $i = 0; $i -lt $args.count; $i++ ) {
-    write-host "Argument  $i is $($args[$i])"
-} 
-dir
-sleep 1
--- src/test/resources/testmovies/Part X/SplitMovie/readme.txt
+++
@@ -1,2 +0,0 @@
-Splitted movies MUST NOT have a space between.
-If there is a space, IT IS a dedicated movie (like HP7 Deathly Hallows Part 1)
